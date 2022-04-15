8b15fa3c4228088d64fd084cf66a512f105a65cb
KPAHAPK <Roman444.zaharov@gmail.com>
2022-04-15 17:01:39 +0300

    My conflict commit

--- app/src/main/java/ru/geekbrains/tests/testprojectwithroman/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/tests/testprojectwithroman/MainActivity.kt
@@ -2,11 +2,13 @@ package ru.geekbrains.tests.testprojectwithroman
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         //что-то написала Таня
+        Toast.makeText(this, "Hi", Toast.LENGTH_SHORT).show()
     }
 }

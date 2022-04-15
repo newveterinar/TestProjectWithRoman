1d0e279311cbc7776eb41a0b2a10967a21f1e18f
Tatiana Goishik <69108210+Tati1909@users.noreply.github.com>
2022-04-15 16:47:45 +0300

    Мой первый комит во второй ветке

--- app/src/main/java/ru/geekbrains/tests/testprojectwithroman/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/tests/testprojectwithroman/MainActivity.kt
@@ -1,11 +1,13 @@
 package ru.geekbrains.tests.testprojectwithroman
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        //jjfjeflhelskjfewj
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //jddjflzsdjflGLIegwat
     }
 }

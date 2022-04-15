657d9d10df06348c3c918758e06ef38afa86d87e
AVKhakhalin <78661461+AVKhakhalin@users.noreply.github.com>
2022-04-15 17:37:48 +0300

    avkhakhalin (#7)
    * Мой первый комит (#1)
    
    * Мой первый комит во второй ветке (#4)
    
    Co-authored-by: newveterinar <103573491+newveterinar@users.noreply.github.com>
    
    * Добавил тост
    
    Co-authored-by: Tatiana Goishik <69108210+Tati1909@users.noreply.github.com>
    Co-authored-by: newveterinar <103573491+newveterinar@users.noreply.github.com>

--- app/src/main/java/ru/geekbrains/tests/testprojectwithroman/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/tests/testprojectwithroman/MainActivity.kt
@@ -1,13 +1,21 @@
 package ru.geekbrains.tests.testprojectwithroman
 
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        //jjfjeflhelskjfewj
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //jddjflzsdjflGLIegwat
+        //что-то написала Таня
+        // Изменения от меня - avkhakhalin
+
+        Toast.makeText(this, "Добавил тост", Toast.LENGTH_SHORT).show()
         //hehsfaewhfwehfowafaw
         //что-то написала Таня
         Toast.makeText(this, "Hi", Toast.LENGTH_SHORT).show()
